e80bc3eb92f255b284942ce263dbfd451502b8e9
liquidCS <144606149+liquidCS@users.noreply.github.com>
2025-07-29 14:31:55 +0800

    Change county geojson to new source, fix error when fetching data using pyrosm

--- building_visualize.py
+++ building_visualize.py
@@ -3,15 +3,15 @@ import os
 import warnings
 import zipfile
 import geopandas 
+import pandas as pd
 import json
-import subprocess
 import pyrosm
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
 
 ## Setting
 
-county_id = 'O'
+county_id = 'F'
 warnings.simplefilter("ignore")
 
 
@@ -36,8 +36,9 @@ MakeSureDirExist(pbf_path)
 geoJson_path = './geojsons'
 MakeSureDirExist(geoJson_path)
 
+county_geoJson_path = './geojsons/county_boundaries.geojson'
 download_files = [
-    {'path': './shp/county_boundaries.zip', 'url': 'https://data.depositar.io/en/dataset/5fae1054-c9d5-409b-96ab-e2bd4da20960/resource/2ad90c25-8c62-42d7-8649-15b107252c52/download/county_boundaries.zip'},
+    {'path': county_geoJson_path, 'url': 'https://data.depositar.io/en/dataset/5fae1054-c9d5-409b-96ab-e2bd4da20960/resource/ac875414-84aa-4d89-916c-3e4e01a4d141/download/county_boundaries_remove_islands.geojson'},
     {'path': './shp/town_boundaries.zip', 'url': 'https://data.depositar.io/en/dataset/5fae1054-c9d5-409b-96ab-e2bd4da20960/resource/c714accb-70f1-45ec-a51d-ce0ef2c81a72/download/town_boundaries.zip'},
     {'path': './shp/village_boundaries.zip', 'url': 'https://data.depositar.io/en/dataset/5fae1054-c9d5-409b-96ab-e2bd4da20960/resource/a44035e1-d829-45fb-8ec5-1f425d3c8cab/download/village_boundaries.zip'},
     {'path': './pbf/taiwan.osm.pbf', 'url': 'https://data.depositar.io/en/dataset/5fae1054-c9d5-409b-96ab-e2bd4da20960/resource/1234dee7-323f-459c-b719-3437bfcb96f3/download/taiwan-20250717.osm.pbf'}
@@ -53,17 +54,11 @@ for i in range(len(download_files)):
 
 ## .shp Data Processing
 
-county_shp_path = './shp/county_boundaries' # Extract county boundaries
 town_shp_path = './shp/town_boundaries' # Extract town boundaries
 village_shp_path = './shp/village_boundaries' # Extract village boundaries
 
-MakeSureDirExist(county_shp_path)
 MakeSureDirExist(town_shp_path)
 MakeSureDirExist(village_shp_path)
-
-if not os.path.exists(f'{county_shp_path}/COUNTY_MOI_1140318.shp'):
-    with zipfile.ZipFile('./shp/county_boundaries.zip', 'r') as zip_ref: # Extract all files in zip to path
-        zip_ref.extractall(county_shp_path)
 
 if not os.path.exists(f'{town_shp_path}/TOWN_MOI_1140318.shp'):
     with zipfile.ZipFile('./shp/town_boundaries.zip', 'r') as zip_ref:
@@ -76,14 +71,9 @@ if not os.path.exists(f'{village_shp_path}/VILLAGE_NLSC_1140620.shp'):
 
 ## geoJson Data Processing
 
-county_geoJson_path = './geojsons/county_boundaries.geojson'
 town_geoJson_path = './geojsons/town_boundaries.geojson'
 village_geoJson_path = './geojsons/village_boundaries.geojson'
 
-for file in os.listdir(county_shp_path): # find the correct shp file
-    if file.endswith('.shp'):
-        county_shapefile = os.path.join(county_shp_path, file)
-        break
 
 for file in os.listdir(town_shp_path):
     if file.endswith('.shp') and file.startswith('TOWN_MOI'):
@@ -96,11 +86,7 @@ for file in os.listdir(village_shp_path):
         break
 
 
-if not os.path.exists(f'{geoJson_path}/county_boundaries.geojson'):
-    county_shp_data = geopandas.read_file(county_shapefile) # Convert shp to geojson 
-    county_shp_data.to_file(county_geoJson_path, driver='GeoJSON')  
-
-if not os.path.exists(f'{geoJson_path}/town_boundaries.geojson'):
+if not os.path.exists(f'{geoJson_path}/town_boundaries.geojson'): # convert shape to geojson
     town_shp_data = geopandas.read_file(town_shapefile)
     town_shp_data.to_file(town_geoJson_path, driver='GeoJSON')
 
@@ -179,6 +165,23 @@ for village_boundary in village_boundaries.iterfeatures():
 
 
 
+def csv_handler(data):
+    csv_path = './out/data/analysis_building&addr.csv'
+    if 'county_id' in data:
+        if os.path.exists(csv_path):
+            df = pd.read_csv(csv_path) 
+
+            if (df['county_id'] == county_id).any():
+                for key, value in data.items():
+                    df.loc[df['county_id'] == county_id, key] = value
+            else:
+                df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
+        else:
+            df = pd.DataFrame(data)
+        df.to_csv(csv_path, index=False)
+    else:
+        print("Error: Didn't not specify county_id in csv_handler's data ")
+        return
 
 
 
@@ -208,7 +211,7 @@ def GenImgBuildingPlot():
 
     plt.savefig(f'{img_path}/{county_id}_buildings_plot.png')
 
-def GenImgAddressAnalyse():
+def GenImgAddressAnalyse(saveCSV=False):
     print("Running Address Analyses...")
 
     address_in_building = []
@@ -220,25 +223,30 @@ def GenImgAddressAnalyse():
 
     for village_boundary in village_boundaries.iterfeatures():
         village_id = village_boundary['properties']['VILLCODE']
+        print(village_boundary['properties']['VILLNAME'], ' - ', village_id)
 
         osm_v = pyrosm.OSM(f"./pbf/village/{village_id}.pbf")
 
-        addresses_t = osm_v.get_data_by_custom_criteria( # Aquire all address points 
-                    custom_filter=address_filter,
-                    keep_nodes=True,
-                    keep_ways=False,
-                    keep_relations=False
-                    )
-        
-        buildings_v = osm_v.get_buildings() # Aquire all buildings
+        try: # may cause error when grabbing data from small island don't know why 
 
-        if addresses_t is not None:
-            for address in addresses_t.iterfeatures():
-                address_p = Point(address['geometry']['coordinates'][0], address['geometry']['coordinates'][1])
-                if buildings_v.contains(address_p).any(): # Detect whether address is inside a building
-                    address_in_building.append(address_p)
-                else:
-                    address_out_building.append(address_p) 
+            addresses_t = osm_v.get_data_by_custom_criteria( # Aquire all address points 
+                        custom_filter=address_filter,
+                        keep_nodes=True,
+                        keep_ways=False,
+                        keep_relations=False
+                        )
+            
+            buildings_v = osm_v.get_buildings() # Aquire all buildings
+
+            if addresses_t is not None and buildings_v is not None:
+                for address in addresses_t.iterfeatures():
+                    address_p = Point(address['geometry']['coordinates'][0], address['geometry']['coordinates'][1])
+                    if buildings_v.contains(address_p).any(): # Detect whether address is inside a building
+                        address_in_building.append(address_p)
+                    else:
+                        address_out_building.append(address_p) 
+        except Exception as e:
+            print(e)
 
     # plot graph
 
@@ -246,7 +254,9 @@ def GenImgAddressAnalyse():
     fig.set_facecolor('black')
     ax.set_axis_off()
 
-    village_boundaries.plot(ax=ax, facecolor='none', edgecolor='black', alpha=0.5, zorder=100) # Plot village boudaries on top
+    # village_boundaries.plot(ax=ax, facecolor='none', edgecolor='black', alpha=0.5, zorder=100) # Plot village boudaries on top
+    boundary = geopandas.read_file(f'{geoJson_path}/county/{county_id}.geojson')
+    boundary.plot(ax=ax, facecolor='none', edgecolor='red', linewidth=5, alpha=0.5)
 
     osm = pyrosm.OSM(f"./pbf/county/{county_id}.pbf")
     roads = osm.get_network(network_type="driving")
@@ -259,9 +269,16 @@ def GenImgAddressAnalyse():
 
     plt.savefig(f'{img_path}/{county_id}_addresses_plot.png')
 
+    if saveCSV:
+        csv_handler({
+            'county_id': [county_id],
+            'addr_in_building': [len(address_in_building)],
+            'addr_out_building': [len(address_out_building)]
+        })
 
 
-def GenImgAddressAnalyseWithBuilding():
+
+def GenImgAddressAnalyseWithBuilding(saveCSV=False):
     print("Running Detail Address Analyses...")
 
     address_filter = {'addr:housenumber': True}
@@ -275,43 +292,69 @@ def GenImgAddressAnalyseWithBuilding():
 
     norm = plt.Normalize(0, 50)
 
-    for village_boundary in village_boundaries.iterfeatures():
+    village_total_count = len(village_boundaries)
+    buildings_total_count = 0
+    addr_total_count = 0
+    addr_detail_count = [0, 0, 0, 0, 0, 0, 0] # 0, 1, 2~5, 6~10, 10~20, 20~50, >50
+    for village_index, village_boundary in enumerate(village_boundaries.iterfeatures()):
         village_id = village_boundary['properties']['VILLCODE']
-        print(village_boundary['properties']['VILLNAME'])
+        print(f"({village_index+1}/{village_total_count})", village_boundary['properties']['VILLNAME'])
 
         osm_v = pyrosm.OSM(f"./pbf/village/{village_id}.pbf")
 
-        addresses_t = osm_v.get_data_by_custom_criteria( # Aquire all address points 
-                    custom_filter=address_filter,
-                    keep_nodes=True,
-                    keep_ways=False,
-                    keep_relations=False
-                    )
-        
-        buildings_v = osm_v.get_buildings() # Aquire all buildings
-        buildings_v['count'] = 0 
+        try: 
 
-        if addresses_t is not None:
-            for address in addresses_t.iterfeatures():
-                address_p = Point(address['geometry']['coordinates'][0], address['geometry']['coordinates'][1])
-                index_in_building = buildings_v.index[buildings_v.contains(address_p)].tolist()
-                for index in index_in_building: # Detect whether address is inside a building
-                    buildings_v['count'][index] += 1
-                    break
+            addresses_t = osm_v.get_data_by_custom_criteria( # Aquire all address points 
+                        custom_filter=address_filter,
+                        keep_nodes=True,
+                        keep_ways=False,
+                        keep_relations=False
+                        )
+            
+            buildings_v = osm_v.get_buildings() # Aquire all buildings
 
-        colors = plt.cm.viridis(norm(buildings_v['count']))
-        buildings_v.plot(ax=ax, color=colors)
+            if addresses_t is not None and buildings_v is not None:
+                buildings_total_count += len(buildings_v)
+                addr_total_count += len(addresses_t)
+                buildings_v['addr_count'] = 0 
+
+                for address in addresses_t.iterfeatures():
+                    address_p = Point(address['geometry']['coordinates'][0], address['geometry']['coordinates'][1])
+                    index_in_building = buildings_v.index[buildings_v.contains(address_p)].tolist()
+                    for index in index_in_building: # Detect whether address is inside a building
+                        buildings_v['addr_count'][index] += 1
+                        break
+
+                colors = plt.cm.viridis(norm(buildings_v['addr_count']))
+                buildings_v.plot(ax=ax, color=colors)
+
         
-        for i in range(len(buildings_v)):
-            building_p = buildings_v['geometry'][i]
-            centroid = building_p.centroid
-            ax.annotate(str(buildings_v['count'][i]), xy=(centroid.x, centroid.y), color='white', fontsize=5, ha='center', zorder=200)
+                ## analysis calcualtion
+                if saveCSV == True:
+                    for i in range(len(buildings_v)):
+                        curr_addr_count = buildings_v['addr_count'][i]
+                        if curr_addr_count == 0:   addr_detail_count[0] += 1
+                        elif curr_addr_count == 1: addr_detail_count[1] += 1
+                        elif curr_addr_count <= 5: addr_detail_count[2] += 1
+                        elif curr_addr_count <= 10:addr_detail_count[3] += 1 
+                        elif curr_addr_count <= 20:addr_detail_count[4] += 1
+                        elif curr_addr_count <= 50:addr_detail_count[5] += 1 
+                        else: addr_detail_count[6] += 1
+
+        except Exception as e:
+                print(e)
+
+
+
+            # ax.text(s=str(buildings_v['count'][i]), x=centroid.x, y=centroid.y, color='gray', fontsize=5, ha='center', zorder=200)
 
         # geopandas.GeoSeries(address_in_building).plot(ax=ax, color='green', markersize=0.5) # Plot address within a building
         # geopandas.GeoSeries(address_out_building).plot(ax=ax, color='red', markersize=0.5) # Plot address without a building
 
 
-    village_boundaries.plot(ax=ax, facecolor='none', edgecolor='white', alpha=0.5, zorder=100) # Plot village boudaries on top
+    # village_boundaries.plot(ax=ax, facecolor='none', edgecolor='white', alpha=0.5, zorder=100) # Plot village boudaries on top
+    boundary = geopandas.read_file(f'{geoJson_path}/county/{county_id}.geojson')
+    boundary.plot(ax=ax, facecolor='none', edgecolor='red', linewidth=5, alpha=0.5)
 
     osm = pyrosm.OSM(f"./pbf/county/{county_id}.pbf")
     roads = osm.get_network(network_type="driving")
@@ -319,12 +362,32 @@ def GenImgAddressAnalyseWithBuilding():
 
     plt.savefig(f'{img_path}/{county_id}_detail_addr_plot.png')
 
+    if saveCSV == True:
+        csv_handler({
+            'county_id': [county_id],
+            'building_count': [buildings_total_count],
+            'address_count': [addr_total_count],
+            '0': [addr_detail_count[0]],
+            '1': [addr_detail_count[1]],
+            '2~5': [addr_detail_count[2]],
+            '6~10': [addr_detail_count[3]],
+            '10~20': [addr_detail_count[4]],
+            '20~50': [addr_detail_count[5]],
+            '>50': [addr_detail_count[6]]
+        })
+
+
+
+
+
+
+
 
 ## Run code plotting code
 
-# GenImgBuildingPlot()
-# GenImgAddressAnalyse()
-GenImgAddressAnalyseWithBuilding()
+GenImgBuildingPlot()
+GenImgAddressAnalyse(saveCSV=True)
+GenImgAddressAnalyseWithBuilding(saveCSV=True)
 
 
 
